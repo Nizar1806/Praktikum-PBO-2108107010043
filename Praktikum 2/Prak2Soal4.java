869f087aaee2e9530bc53b0a5ab7f404347ae113
Muhammad Nizar Asykary <112839806+Nizar1806@users.noreply.github.com>
2022-09-13 21:31:48 +0700

    Soal 4 selesai

---
+++ Praktikum 2/Prak2Soal4.java
@@ -0,0 +1,11 @@
+public class Prak2Soal04 {
+
+    public static void main(String[] args) {
+
+        for (int a=2; a<=20; a+=2){
+
+            System.out.print(a+" ");
+        }
+    }
+        
+}
